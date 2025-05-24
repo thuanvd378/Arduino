723df27d5b0c0abbb43ccc79d15c0bf959df5321
Vũ Đức Thuận <86833744+thuanvd378@users.noreply.github.com>
2025-05-24 12:09:03 +0700

    Add files via upload

---
+++ Project_ESY/Project ESY/Project ESY/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace Project_ESY
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbPorts = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.colID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            this.rtbConsole = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbPorts
+            // 
+            this.cmbPorts.FormattingEnabled = true;
+            this.cmbPorts.Location = new System.Drawing.Point(72, 94);
+            this.cmbPorts.Name = "cmbPorts";
+            this.cmbPorts.Size = new System.Drawing.Size(152, 24);
+            this.cmbPorts.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(228, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(424, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Chương trình Check-in vân tay";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "COM";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(239, 94);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(96, 30);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Tải lại";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(341, 94);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(98, 30);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Kết nối";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colID,
+            this.colTime});
+            this.dgvData.Location = new System.Drawing.Point(254, 189);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.RowHeadersWidth = 51;
+            this.dgvData.RowTemplate.Height = 24;
+            this.dgvData.Size = new System.Drawing.Size(610, 283);
+            this.dgvData.TabIndex = 5;
+            // 
+            // colID
+            // 
+            this.colID.HeaderText = "ID";
+            this.colID.MinimumWidth = 6;
+            this.colID.Name = "colID";
+            this.colID.ReadOnly = true;
+            this.colID.Width = 150;
+            // 
+            // colTime
+            // 
+            this.colTime.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colTime.HeaderText = "Thời gian truy cập";
+            this.colTime.MinimumWidth = 6;
+            this.colTime.Name = "colTime";
+            this.colTime.ReadOnly = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(774, 478);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 51);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Xuất Excel";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // rtbConsole
+            // 
+            this.rtbConsole.Location = new System.Drawing.Point(12, 189);
+            this.rtbConsole.Name = "rtbConsole";
+            this.rtbConsole.ReadOnly = true;
+            this.rtbConsole.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.rtbConsole.Size = new System.Drawing.Size(227, 283);
+            this.rtbConsole.TabIndex = 7;
+            this.rtbConsole.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(14, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Serial Monitor :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 541);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rtbConsole);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbPorts);
+            this.Name = "Form1";
+            this.Text = "Project ESY";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbPorts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTime;
+        private System.Windows.Forms.RichTextBox rtbConsole;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Project_ESY/Project ESY/Project ESY/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_ESY
+{
+    public partial class Form1 : Form
+    {
+        SerialPort _serial;
+        bool _readySeen = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadSerialPorts();
+        }
+
+        private void LoadSerialPorts()
+        {
+            cmbPorts.Items.Clear();
+            var ports = SerialPort.GetPortNames();
+            Array.Sort(ports);
+            cmbPorts.Items.AddRange(ports);
+            if (ports.Length > 0) cmbPorts.SelectedIndex = 0;
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e) => LoadSerialPorts();
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (_serial != null && _serial.IsOpen)
+            {
+                Disconnect();
+                btnConnect.Text = "Kết nối";
+                return;
+            }
+
+            if (cmbPorts.SelectedItem == null) return;
+            string portName = cmbPorts.SelectedItem.ToString();
+            _serial = new SerialPort(portName, 115200)
+            {
+                NewLine = "\n",
+                DtrEnable = false,
+                RtsEnable = false,
+                ReadTimeout = 1000,
+                WriteTimeout = 1000
+            };
+            _serial.DataReceived += Serial_DataReceived;
+            try
+            {
+                _serial.Open();
+                btnConnect.Text = "Ngắt kết nối";
+                Task.Delay(2000).ContinueWith(_ =>
+                {
+                    if (_serial.IsOpen && !_readySeen)
+                        SafeWriteAndEcho("GETDATA\n");
+                });
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Cổng đang bị ứng dụng khác giữ, hãy đóng ứng dụng đó.",
+                                "Access denied", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Disconnect();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Không thể mở cổng: " + ex.Message);
+                Disconnect();
+            }
+        }
+
+        private void Disconnect()
+        {
+            if (_serial != null)
+            {
+                _serial.DataReceived -= Serial_DataReceived;
+                if (_serial.IsOpen) _serial.Close();
+                _serial.Dispose();
+                _serial = null;
+            }
+        }
+
+        private void SafeWrite(string s)
+        {
+            try { if (_serial?.IsOpen == true) _serial.Write(s); }
+            catch { /* ignore */ }
+        }
+
+        private void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string line = _serial.ReadLine().Trim();
+                AppendConsole(line);
+                if (line == "READY") { _readySeen = true; SafeWriteAndEcho("GETDATA\n"); return; }
+                if (line == "END_OF_DATA" || line.StartsWith("ERR")) return;
+
+                var parts = line.Split(',');
+                if (parts.Length == 2)
+                {
+                    string id = parts[0];
+                    string time = parts[1];
+                    Invoke(new Action(() => dgvData.Rows.Add(id, time)));
+                }
+            }
+            catch (TimeoutException) { /* ignore */ }
+            finally{}
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            Disconnect();
+            base.OnFormClosing(e);
+        }
+        private void AppendConsole(string text)
+        {
+            if (rtbConsole.InvokeRequired)
+            {
+                rtbConsole.Invoke(new Action<string>(AppendConsole), text);
+                return;
+            }
+            // Giới hạn tối đa 5000 dòng để không phình RAM
+            if (rtbConsole.Lines.Length > 5000)
+                rtbConsole.Clear();
+
+            rtbConsole.AppendText(text + Environment.NewLine);
+            rtbConsole.SelectionStart = rtbConsole.TextLength;
+            rtbConsole.ScrollToCaret();
+        }
+
+        private void SafeWriteAndEcho(string s)
+        {
+            AppendConsole("→ " + s.Trim());   // hiển thị lệnh gửi
+            SafeWrite(s);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dgvData.Rows.Count == 0)
+            {
+                MessageBox.Show("Chưa có dữ liệu để xuất.");
+                return;
+            }
+            using (var sfd = new SaveFileDialog { Filter = "CSV file (*.csv)|*.csv", FileName = "log.csv" })
+            {
+                if (sfd.ShowDialog() != DialogResult.OK) return;
+                try
+                {
+                    using (var sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+                    {
+                        var headers = dgvData.Columns.Cast<DataGridViewColumn>()
+                                          .Select(c => $"\"{c.HeaderText}\"");
+                        sw.WriteLine(string.Join(",", headers));
+                        foreach (DataGridViewRow row in dgvData.Rows)
+                        {
+                            if (row.IsNewRow) continue;
+                            var cells = row.Cells.Cast<DataGridViewCell>()
+                                           .Select(c => c.Value == null ? "" : $"\"{c.Value.ToString().Replace("\"", "\"\"")}\"");
+                            sw.WriteLine(string.Join(",", cells));
+                        }
+                    }
+                    MessageBox.Show("Xuất dữ liệu thành công!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi xuất: " + ex.Message);
+                }
+            }
+        }
+    }
+}
